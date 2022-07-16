36a9078d54ef7b057cd922d172a16780d340a7ae
Camilo Torres <racknarow1@gmail.com>
2022-07-16 00:26:49 -0500

    add context

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
 import { Container, Grid, Typography } from '@mui/material';
 import Formulario from './components/Formulario';
+import { NoticiasProvider } from './context/NoticiasProvider';
 
 function App() {
     return (
-        <Container>
-            <header>
-                <Typography
-                    align="center"
-                    marginY={5}
-                    component="h1"
-                    variant="h3"
-                >
-                    Buscador de Noticias
-                </Typography>
-            </header>
+        <NoticiasProvider>
+            <Container>
+                <header>
+                    <Typography
+                        align="center"
+                        marginY={5}
+                        component="h1"
+                        variant="h3"
+                    >
+                        Buscador de Noticias
+                    </Typography>
+                </header>
 
-            <Grid
-                container
-                direction="row"
-                justifyContent="center"
-                alignItems="center"
-            >
-                <Grid item xs={12} md={6} lg={4}>
-                    <Formulario />
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    <Grid item xs={12} md={6} lg={4}>
+                        <Formulario />
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Container>
+            </Container>
+        </NoticiasProvider>
     );
 }
 
