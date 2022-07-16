4a7b15aab42047e860472e8ea245a5c192d15e97
Camilo Torres <racknarow1@gmail.com>
2022-07-16 00:51:02 -0500

    add fetch noticias

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -1,11 +1,28 @@
 import { useState, useEffect, createContext } from 'react';
+import axios from 'axios';
 
 const NoticiasContext = createContext();
 
 
 const NoticiasProvider = ({ children }) => {
 
+    const apiKey = "3dbd69f687aa4441bc8352ee8caa6980";
     const [categoria, setCategoria] = useState('general');
+    const [noticias, setNoticias] = useState([]);
+
+
+    //! consultarAPI
+    useEffect(() => {
+        const consultarAPI = async () => {
+            const url = `https://newsapi.org/v2/top-headlines?country=co&category=${categoria}&apiKey=${apiKey}`;
+
+            const data = await axios.get(url);
+            setNoticias(data.data.articles);
+            
+
+        }
+        consultarAPI();
+    }, [categoria]);
 
     const handleChangeCategoria = (e) => {
         setCategoria(e.target.value);
