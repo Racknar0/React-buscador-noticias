27d4fbe034629308c65ba8b5e8eba5203e45eb69
Camilo Torres <racknarow1@gmail.com>
2022-07-16 00:17:28 -0500

    add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
-import { Container, Grid, Typography } from "@mui/material"
-
+import { Container, Grid, Typography } from '@mui/material';
+import Formulario from './components/Formulario';
 
 function App() {
+    return (
+        <Container>
+            <header>
+                <Typography
+                    align="center"
+                    marginY={5}
+                    component="h1"
+                    variant="h3"
+                >
+                    Buscador de Noticias
+                </Typography>
+            </header>
 
-
-  return (
-    <Container >
-      <header>
-        <Typography
-          align="center"
-          marginY={5}
-          component="h1"
-          variant="h3"
-        >
-          Buscador de Noticias
-        </Typography>
-      </header>
-    </Container>
-  )
+            <Grid
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Grid item xs={12} md={6} lg={4}>
+                    <Formulario />
+                </Grid>
+            </Grid>
+        </Container>
+    );
 }
 
-export default App
+export default App;
