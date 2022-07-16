1ffb1e900726400856d3436da520ca8f9b7e3eeb
Camilo Torres <racknarow1@gmail.com>
2022-07-16 00:02:55 -0500

    import MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { Container, Grid, Typography } from "@mui/material"
+
 
 function App() {
 
 
   return (
-    <h1>App noticias</h1>
+    <Container >
+      <header>
+        <Typography
+          align="center"
+          marginY={5}
+          component="h1"
+          variant="h3"
+        >
+          Buscador de Noticias
+        </Typography>
+      </header>
+    </Container>
   )
 }
 
